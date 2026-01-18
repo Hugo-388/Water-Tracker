579935a8c6ba378067c2eb0c8d6f1ea9d4057d72
Hugo <hugomx@sfr.fr>
2026-01-18 19:40:42 +0100

    Modif warning version 1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { WaterProgressComponent } from './ui/water-progress/water-progress.component';
 import { WaterControlsComponent } from './ui/water-controls/water-controls.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, WaterProgressComponent, WaterControlsComponent],
+  imports: [WaterProgressComponent, WaterControlsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
